da81da39b53fe6bb79ec3b7d645a2e5a66699229
marksweiss <marksimonweiss@gmail.com>
2018-12-02 00:55:02 -0500

    Fix bug in fft on line 99 of extra/fft.py. Was breaking both tests in test_fft.py.

--- mingus/extra/fft.py
+++ mingus/extra/fft.py
@@ -96,7 +96,7 @@ def find_frequencies(data, freq=44100, bits=16):
     uniquePts = numpy.ceil((n + 1) / 2.0)
 
     # Scale by the length (n) and square the value to get the amplitude
-    p = [(abs(x) / float(n)) ** 2 * 2 for x in p[0:uniquePts]]
+    p = [(abs(x) / float(n)) ** 2 * 2 for x in p[0:int(uniquePts)]]
     p[0] = p[0] / 2
     if n % 2 == 0:
         p[-1] = p[-1] / 2
